5a24fadbfa5c8dff2e4b76bd1bbf6efd60ea68ca
margaret <margaret.kenda@yahoo.com>
2022-08-05 00:14:32 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,24 @@
+const add = (a,b)=> {
+return(a+b)
+}
+const subtract = (a,b)=> {
+return(a-b)
+}
+const multiply = (a,b)=>{
+    return(a*b)
+}
+const divide = (a,b)=>{
+    return(a/b)
+}
+const increment = (a)=>{
+    return (a+1)
+}
+const decrement = (a)=>{
+    return(a-1)
+}
+const makeInt = (a)=>{
+    return parseInt(a,10)
+}
+const preserveDecimal = (a)=>{
+    return parseFloat(a)
+}
